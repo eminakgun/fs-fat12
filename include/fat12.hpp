ab753a40443fd4e4e03a6ef772fdf933d6409eb9
eminakgun <eminakgn@outlook.com>
2024-06-08 14:34:48 +0300

    Add Directory iterator

--- include/fat12.hpp
+++ include/fat12.hpp
@@ -22,7 +22,7 @@ namespace fat12 {
         int total_size_bytes;
         int total_size_kb;
         int fat_size_bytes;
-        int entry_cnt_in_block;
+        
         
         // Start addresses
         int fat1_start;
@@ -53,9 +53,12 @@ namespace fat12 {
         bool is_in_root(DirectoryEntry* dir);
         
     public:
+    int entry_cnt_in_block;
+
+    
         fat12_fs(string name):name(name){};
         ~fat12_fs(){ dump_fs(); delete[] fs_buffer;};
-        
+
         void traverse_all();
         void dump_fs();
         void create_fs(int size_kb);
@@ -69,6 +72,60 @@ namespace fat12 {
         // Function to read a FAT entry
         FatEntry read_fat_entry(uint16_t cluster);
         void write_fat_entry(uint16_t cluster, FatEntry value);
+
+        friend class Fat12Iterator;
+        class Fat12Iterator {
+        private:
+            fat12_fs* fs;
+            DirectoryEntry* root_entry;
+            DirectoryEntry* current_cluster;
+            uint16_t current_cluster_num;
+            int current_idx;
+            int fat_idx;
+
+            void load_cluster(uint16_t cluster) {
+                auto cluster_start = cluster * fs->block_size_byte;
+                current_cluster = reinterpret_cast<DirectoryEntry*>(&(fs->data_area[cluster_start]));
+            }
+
+            void advance_cluster() {
+                current_cluster_num = fs->FAT[fat_idx];
+                current_idx = 0;
+                load_cluster(current_cluster_num);
+            }
+
+        public:
+            Fat12Iterator(DirectoryEntry* entry, fat12_fs* fs) 
+                : root_entry(entry), fs(fs), current_cluster_num(entry->starting_cluster), current_idx(0) {
+                if (is_directory(*root_entry)) {
+                    auto fat_idx = entry->starting_cluster;
+                    auto cluster_num = fat_idx;
+                    check_fat_idx(fat_idx);
+                    load_cluster(current_cluster_num);
+                }
+            }
+
+            bool has_next() {
+                return (current_idx < (fs->entry_cnt_in_block))
+                            || !is_last_cluster(fs->FAT[fat_idx]);
+            }
+
+            DirectoryEntry* next() {
+                if (!is_directory(*root_entry)) {
+                    return nullptr;
+                }
+
+                if (current_idx >= fs->entry_cnt_in_block) {
+                    advance_cluster();
+                }
+
+                return &current_cluster[current_idx++];
+            }
+        };
+
+        Fat12Iterator* iterator(DirectoryEntry* entry){
+            return new Fat12Iterator(entry, this);
+        }
     };
 
 }//namespace
